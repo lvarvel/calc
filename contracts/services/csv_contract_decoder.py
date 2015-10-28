ab962fbeeeff3d312f6c3c867f34c38970d5bd38
Bin He and Richard Rode <pair+bhe+rrode@pivotal.io>
2015-10-28 15:23:50 -0400

    Create CSVContractDecoder that parses standard CSV files into Contract models

---
+++ contracts/services/csv_contract_decoder.py
@@ -0,0 +1,60 @@
+from contracts.models import Contract
+from django.utils.datetime_safe import datetime
+import pandas as pd
+
+FEDERAL_MIN_CONTRACT_RATE = 10.10
+
+class CSVContractDecoder():
+    def __init__(self, file_name):
+        self.data_frame = pd.read_csv(file_name).fillna('')
+
+    def decode(self):
+        contracts = []
+
+        for _, row in self.data_frame.iterrows():
+
+            if not (row['Labor Category'] and row['Contract Year'] and row['Year 1/base']):
+                continue
+
+            contract = Contract()
+            contract.idv_piid = row['CONTRACT .']
+            contract.labor_category = row['Labor Category'].strip().replace('\n', ' ')
+            contract.vendor_name = row['COMPANY NAME']
+            contract.education_level = contract.get_education_code(row['Education'])
+            contract.schedule = row['Schedule']
+            contract.business_size = row['Bus Size']
+            contract.contract_year = row['Contract Year']
+            contract.sin = row['SIN NUMBER']
+            contract.hourly_rate_year1 = contract.normalize_rate(str(row['Year 1/base']))
+            contract.contractor_site = row['Location']
+
+            if row['Begin Date']:
+                contract.contract_start = datetime.strptime(row['Begin Date'], '%m/%d/%Y').date()
+            if row['End Date']:
+                contract.contract_end = datetime.strptime(row['End Date'], '%m/%d/%Y').date()
+
+            contract.min_years_experience = row['MinExpAct'] if row['MinExpAct'] else 0
+
+            for count, rate in enumerate(row[2:6]):
+                if rate:
+                    setattr(contract, 'hourly_rate_year{}'.format(count + 2), contract.normalize_rate(str(rate)))
+
+            self.__generate_contract_rate_years(row, contract)
+
+            contracts.append(contract)
+
+        return contracts
+
+    def __generate_contract_rate_years(self, row, contract):
+        price_fields = {
+            'current_price': getattr(contract, 'hourly_rate_year{}'.format(row['Contract Year']), 0)
+        }
+
+        if row['Contract Year'] < 5:
+            price_fields['next_year_price'] = getattr(contract, 'hourly_rate_year{}'.format(row['Contract Year'] + 1), 0)
+        if row['Contract Year'] < 4:
+            price_fields['second_year_price'] = getattr(contract, 'hourly_rate_year{}'.format(row['Contract Year'] + 2), 0)
+
+        for field, price in price_fields.items():
+            if price and price >= FEDERAL_MIN_CONTRACT_RATE:
+                setattr(contract, field, price)
---
+++ contracts/tests/csv_contract_decoder_tests.py
@@ -0,0 +1,57 @@
+from contracts.models import Contract
+from django.test import TestCase
+from django.utils.datetime_safe import datetime
+from tempfile import NamedTemporaryFile
+from contracts.services.csv_contract_decoder import CSVContractDecoder
+
+
+class CsvDecoderTests(TestCase):
+    def setUp(self):
+        self.csv_file = NamedTemporaryFile(suffix='.csv')
+
+        self.csv_file.write(
+            b'Labor Category,Year 1/base,Year 2,Year 3,Year 4,Year 5,Education,MinExpAct,Bus Size,'
+            b'Location,COMPANY NAME,CONTRACT .,Schedule,SIN NUMBER,Contract Year,Begin Date,End Date\n'
+        )
+        self.csv_file.write(
+            b'21071-Order Filler,38.22,38.99,39.77,40.56,41.37,High School,1,O,Both,"Alutiiq Global Solutions, LLC",'
+            b'GS-10F-0152P,Logistics,"874-501, 874-503, 874-504, 874-505, 874-507",2,1/9/2004,1/8/2019\n')
+        self.csv_file.write(
+            b'Analyst/Consultant I,66.51,,,,,Bachelors,1,O,,"Enterprise Information Services, Incorporated (d.b.a.) '
+            b'Eis",GS-00F-182CA,Consolidated,"C874 1, C874 7",1,,\n'
+        )
+
+        self.csv_file.flush()
+
+    def tearDown(self):
+        self.csv_file.close()
+
+    def test_decoder_creates_data_frame_from_file(self):
+        csv_decoder = CSVContractDecoder(self.csv_file.name)
+
+        self.assertEqual(csv_decoder.data_frame['Labor Category'][0], '21071-Order Filler')
+        self.assertEqual(csv_decoder.data_frame['Year 5'][0], 41.37)
+
+    def test_decoder_returns_array_of_contracts(self):
+        contracts = CSVContractDecoder(self.csv_file.name).decode()
+
+        self.assertEqual(len(contracts), 2)
+        self.assertIsInstance(contracts[0], Contract)
+
+    def test_decoder_creates_contract_object(self):
+        contracts = CSVContractDecoder(self.csv_file.name).decode()
+
+        self.assertEqual(contracts[0].education_level, 'HS')
+        self.assertEqual(contracts[0].contract_end, datetime.strptime('1/8/2019', '%m/%d/%Y').date())
+        self.assertEqual(contracts[1].contract_end, None)
+        self.assertEqual(contracts[1].labor_category, 'Analyst/Consultant I')
+
+    def test_decoder_calculates_contract_years(self):
+        contracts = CSVContractDecoder(self.csv_file.name).decode()
+
+        self.assertEqual(contracts[0].hourly_rate_year1, 38.22)
+        self.assertEqual(contracts[0].hourly_rate_year2, 38.99)
+        self.assertEqual(contracts[1].hourly_rate_year1, 66.51)
+        self.assertEqual(contracts[1].hourly_rate_year4, None)
+        self.assertEqual(contracts[0].next_year_price, 39.77)
+        self.assertEqual(contracts[0].second_year_price, 40.56)
